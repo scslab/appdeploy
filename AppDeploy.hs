5f296778b7212dce51d5aa7c894fa811e260aed8
Amit Aryeh Levy <amit@amitlevy.com>
2013-09-10 17:33:53 -0700

    Cleanup with Amy

--- AppDeploy.hs
+++ AppDeploy.hs
@@ -1,155 +1,67 @@
 {-# LANGUAGE OverloadedStrings, TemplateHaskell, DeriveDataTypeable #-}
 
-import System.Posix.Process
-import System.Posix.Unistd
-import System.Posix.Types
-import Control.Concurrent
-import Data.List.Split
-import Data.IORef
-import Data.Global
-import Control.Concurrent.Spawn
-import System.IO.Unsafe
-import qualified Data.HashTable as H
-import Network
-import System.IO
 import Control.Exception
 import Control.Monad
-import Control.Monad.Trans
-import Control.Exception.Base
-
+import System.Process
+import Control.Concurrent
+import Data.List.Split
+import qualified Data.HashTable.IO as H
+import Network
+import System.IO
+import System.IO.Unsafe
 import Debug.Trace
 
--- Step 1
--- taskctl /path/to/app [arg1 [arg2 [arg3]]]
--- look at .env file to get environment variables (?)
-
--- Step 2
--- taskctl description_file
--- File looks like:
--- /path/to/app1 [arg1 [arg2 [...]]]
--- /path/to/app2 [arg1 [...]]
--- ...
-
--- Step 2
--- Turn into TCP or HTTP server that can monitor multiple apps
-
--- Step 3
--- List status of running processes
-
-
---type HashTable k v = H.BasicHashTable k v
-
-ht :: (H.HashTable String String)
+ht :: (H.BasicHashTable String ProcessHandle)
 {-# NOINLINE ht #-}
-ht = unsafePerformIO $ H.new (==) H.hashString
+ht = unsafePerformIO $ H.new
 
---main :: FilePath -> IO ()
+main :: IO ()
 main = bracket (listenOn $ PortNumber 1234) sClose $ \s -> do
-  htMutex <- newMutex
-  --trace "releaseMutex" $ releaseMutex htMutex
-  processesString <- trace "processing file" $ readFile "./processes" 
-  let processList = lines processesString
-  mapM (taskctl htMutex) processList
+  htMutex <- newMVar ()
   forever $ do
     (handle, hostname, portnum) <- accept s
-    forkIO $ respondStatuses handle htMutex
+    forkIO $ handleConnection handle htMutex
 
 
---main = do 
---    putStrLn "hello"
-
-    --startApp "/aadfasdf" False ["a"] (Just [("s", "c")])
-
-parseEnv :: FilePath -> IO (Maybe [(String, String)])
-parseEnv envFile = do
-    envString <- readFile envFile
+parseEnv :: String -> [(String, String)]
+parseEnv envString =
     let varList = lines envString
-    return $ Just $ map (\line -> ((head $ splitOn "=" line) ,(last $ splitOn "=" line))) varList  
+    in map parseLine varList  
+    where parseLine line =
+            let (key:value:[]) = splitOn "=" line
+            in (key, value)
 
---readProcesses :: FilePath -> IO a 
---readProcesses descriptionFile = do
-    
---    processesString <- readFile descriptionFile 
---    let processList = lines processesString
---    mapM (taskctl htMutex) processList
-
-taskctl :: Mutex -> String -> IO ThreadId
-taskctl htMutex cline = do
-    let entries = splitOn " " cline
-    let command = head entries
-    let args = tail entries
-    env <- parseEnv "./.env"
-    startApp htMutex command False args env
-
-
-
-
-startApp :: Mutex -> FilePath             -- ^ Command
-            -> Bool             -- ^ Search PATH?
-            -> [String]             -- ^ Arguments
-            -> Maybe [(String, String)]     -- ^ Environment
-            -> IO ThreadId
-startApp htMutex command spath args env  = forkIO $ go 
-        where go = do
-                --ht1 <- ht
-                --ht2 <- readIORef ht1
-
-                claimMutex htMutex
-                success <- H.update ht (show command) "running"
-                if not success
-                then  H.insert ht (show command) "running"
-                else return ()
-                releaseMutex htMutex
-                err1 <- spawn (executeFile command spath args env)
-                claimMutex htMutex
-                H.update ht (show command) "down"
-                releaseMutex htMutex
-                err <- err1
-                case err of
-                    Left (SomeException e) -> putStrLn (show e)
-                    _ -> return ()
-                go
-        --pid <- forkProcess (executeFile command spath args env) 
-        --checkStatus pid command spath args env
-
-
-
-respondStatuses :: Handle -> Mutex -> IO ()
-respondStatuses h htMutex= do
-    --ht1 <- ht
-    --ht2 <- readIORef ht1
+handleConnection :: Handle -> MVar () -> IO ()
+handleConnection h htMutex = forever $ do
     hPutStrLn h "Enter Command: "
     input <- hGetLine h 
-    --determine command and respond appropriately
-    let cmd = head $ words input
-    let args = tail $ words input
-    env <- parseEnv "./.env"
+    let (cmd:args) = words input
     case cmd of
         "statuses" -> do
-            claimMutex htMutex
-            statusList <- H.toList ht 
-            releaseMutex htMutex
-            hPutStrLn h (show (statusList :: [(String, String)]))
+            statusList <- withMVar htMutex $ \_ -> H.toList ht 
+            hPutStrLn h (show $ map fst statusList)
         "launch" -> do
+            let env = [] --something from the request
             startApp htMutex (head args) False (tail args) env
             return ()
-        "setEnvFile" -> do
-            newEnv <- parseEnv (head args)
-            startApp htMutex (head args) False (tail args) newEnv
-            return ()
+        "kill" -> do
+            let key = head args
+            mPHandle <- withMVar htMutex $ \_ -> H.lookup ht key 
+            case mPHandle of
+              Nothing -> hPutStrLn h "No process found"
+              Just pHandle -> terminateProcess pHandle
         _ -> do
             hPutStrLn h "invalid command"
-    respondStatuses h htMutex                
 
+startApp :: MVar () -> FilePath             -- ^ Command
+            -> Bool             -- ^ Search PATH?
+            -> [String]             -- ^ Arguments
+            -> [(String, String)]     -- ^ Environment
+            -> IO ThreadId
+startApp htMutex command spath args env  = forkIO $ do 
+    let createProc = proc command args
+    (_, _, _, pHandle) <- createProcess createProc
+    withMVar htMutex $ \_ -> H.insert ht command pHandle
+    waitForProcess pHandle
+    withMVar htMutex $ \_ -> H.delete ht command
 
-
-type Mutex = MVar ()
-
-claimMutex :: MonadIO m => Mutex -> m ()
-claimMutex = liftIO . takeMVar
-
-newMutex :: MonadIO m => m Mutex
-newMutex = liftIO $ newMVar ()
-
-releaseMutex :: MonadIO m => Mutex -> m ()
-releaseMutex m = liftIO $ putMVar m ()
