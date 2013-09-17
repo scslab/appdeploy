d6fbdefa8506e5e9412fbc5410908bccd2049023
Amit Aryeh Levy <amit@amitlevy.com>
2013-09-17 15:37:32 -0700

    Notes for controller

--- src/appcontroller.hs
+++ src/appcontroller.hs
@@ -2,3 +2,11 @@ module Main where
 
 main :: IO ()
 main = return ()
+
+-- read tar files in & send over network
+-- accept commands: "run this app"
+-- communicate w/ app deployer about statuses of apps
+-- load balancing
+-- kill commands
+-- have some way of knowing the statuses once this program dies/restarts
+-- know the availability of the deployers
